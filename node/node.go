38bd3fd7fa4107e65ca18b1f0d5b0fdda370d436
nyarla <nyarla@kalaclista.com>
2023-12-24 15:23:52 +0900

    refactor(node): change api

--- node/node.go
+++ node/node.go
@@ -1,52 +1,222 @@
 package node
 
 import (
-	"fmt"
-	"html"
+	"sort"
 	"strings"
 )
 
-type Raw string
+type NodeType int
 
-func Text(src string) Raw {
-	return Raw(html.EscapeString(src))
+const (
+	TextNode NodeType = iota
+	AttrNode
+	ElementNode
+)
+
+type NodeWriter interface {
+	Type() NodeType
+	WriteNode(w *strings.Builder) (int, error)
 }
 
-type Attr string
+type TextNodeWriterFunc func(w *strings.Builder) (int, error)
 
-func Pair(name, value string) Attr {
-	return Attr(fmt.Sprintf(`%s="%s"`, html.EscapeString(name), html.EscapeString(value)))
+func (f TextNodeWriterFunc) Type() NodeType { return TextNode }
+func (f TextNodeWriterFunc) WriteNode(w *strings.Builder) (int, error) {
+	return f(w)
 }
 
-func Flag(name string) Attr {
-	return Attr(html.EscapeString(name))
+type AttrNodeWriterFunc func(w *strings.Builder) (int, error)
+
+func (f AttrNodeWriterFunc) Type() NodeType { return AttrNode }
+func (f AttrNodeWriterFunc) WriteNode(w *strings.Builder) (int, error) {
+	return f(w)
 }
 
-func Node(el string, contains ...interface{}) Raw {
-	if len(contains) == 0 {
-		return Raw(fmt.Sprintf(`<%s />`, html.EscapeString(el)))
+type ElementNodeWriterFunc func(w *strings.Builder) (int, error)
+
+func (f ElementNodeWriterFunc) Type() NodeType { return ElementNode }
+func (f ElementNodeWriterFunc) WriteNode(w *strings.Builder) (int, error) {
+	return f(w)
+}
+
+func Raw(src string) NodeWriter {
+	return TextNodeWriterFunc(func(w *strings.Builder) (int, error) {
+		return w.WriteString(src)
+	})
+}
+
+var htmlReplacer = strings.NewReplacer(
+	`&`, `&amp;`,
+	`>`, `&gt;`,
+	`<`, `&lt;`,
+	`"`, `&#34;`,
+	`'`, `&#39;`,
+	"`", `&#96;`,
+	`{`, `&#123;`,
+	`}`, `&#125;`,
+)
+
+func Text(src string) NodeWriter {
+	return TextNodeWriterFunc(func(w *strings.Builder) (int, error) {
+		return htmlReplacer.WriteString(w, src)
+	})
+}
+
+func Attr(kv ...string) NodeWriter {
+	if len(kv) == 1 {
+		return AttrNodeWriterFunc(func(w *strings.Builder) (int, error) {
+			return htmlReplacer.WriteString(w, kv[0])
+		})
 	}
 
-	attrs := make([]string, 0, len(contains))
-	contents := make([]string, 0, len(contains))
+	if len(kv)%2 != 0 {
+		panic(`node.Attr: odd argument count`)
+	}
 
-	for _, contain := range contains {
-		if data, ok := contain.(Attr); ok {
-			attrs = append(attrs, string(data))
-			continue
+	return AttrNodeWriterFunc(func(w *strings.Builder) (total int, throw error) {
+		for idx := 0; idx < len(kv); idx = idx + 2 {
+			var (
+				count int
+				err   error
+			)
+
+			count, err = htmlReplacer.WriteString(w, kv[idx])
+			total += count
+			if err != nil {
+				throw = err
+				return
+			}
+
+			count, err = w.WriteString(`=`)
+			total += count
+			if err != nil {
+				throw = err
+				return
+			}
+			count, err = w.WriteString(`"`)
+			total += count
+			if err != nil {
+				throw = err
+				return
+			}
+
+			count, err = htmlReplacer.WriteString(w, kv[idx+1])
+			total += count
+			if err != nil {
+				throw = err
+				return
+			}
+
+			count, err = w.WriteString(`"`)
+			total += count
+			if err != nil {
+				throw = err
+				return
+			}
 		}
 
-		if data, ok := contain.(Raw); ok {
-			contents = append(contents, string(data))
-			continue
+		return
+	})
+}
+
+type ByNodeType []NodeWriter
+
+func (a ByNodeType) Len() int      { return len(a) }
+func (a ByNodeType) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByNodeType) Less(i, j int) bool {
+	if a[i].Type() == AttrNode {
+		return true
+	}
+
+	return i < j
+}
+
+func Element(el string, contains ...NodeWriter) NodeWriter {
+	sort.Sort(ByNodeType(contains))
+	return ElementNodeWriterFunc(func(w *strings.Builder) (total int, throw error) {
+		var (
+			count int
+			err   error
+		)
+
+		// begin tag start
+		count, err = w.WriteString(`<`)
+		total += count
+		if err != nil {
+			throw = err
+			return
 		}
 
-		panic(fmt.Sprintf(`Unsupported %+v`, contain))
-	}
+		// write tag name
+		count, err = htmlReplacer.WriteString(w, el)
+		total += count
+		if err != nil {
+			throw = err
+			return
+		}
 
-	if len(contents) == 0 {
-		return Raw(fmt.Sprintf(`<%s %s />`, html.EscapeString(el), strings.Join(attrs, " ")))
-	}
+		// write contains
+		for idx, contain := range contains {
+			if (idx == 0 && contain.Type() == TextNode || contain.Type() == ElementNode) || (idx > 0 && contains[idx-1].Type() == AttrNode && (contain.Type() == TextNode || contain.Type() == ElementNode)) {
+				count, err = w.WriteString(`>`)
+				total += count
+				if err != nil {
+					throw = err
+					return
+				}
+			}
 
-	return Raw(fmt.Sprintf(`<%s %s>%s</%s>`, html.EscapeString(el), strings.Join(attrs, " "), strings.Join(contents, ""), html.EscapeString(el)))
+			if contain.Type() == AttrNode {
+				count, err = w.WriteString(` `)
+				total += count
+				if err != nil {
+					throw = err
+					return
+				}
+			}
+
+			count, err = contain.WriteNode(w)
+			total += count
+			if err != nil {
+				throw = err
+				return
+			}
+		}
+
+		// close tag if content is empty
+		if len(contains) == 0 || contains[len(contains)-1].Type() == AttrNode {
+			count, err = w.WriteString(` />`)
+			total += count
+			if err != nil {
+				throw = err
+				return
+			}
+
+			return
+		}
+
+		// close tag
+		count, err = w.WriteString(`</`)
+		total += count
+		if err != nil {
+			throw = err
+			return
+		}
+
+		count, err = htmlReplacer.WriteString(w, el)
+		total += count
+		if err != nil {
+			throw = err
+			return
+		}
+
+		count, err = w.WriteString(`>`)
+		total += count
+		if err != nil {
+			throw = err
+			return
+		}
+
+		return
+	})
 }
--- node/node_test.go
+++ node/node_test.go
@@ -1,47 +1,219 @@
 package node
 
-import "testing"
+import (
+	"html"
+	"strings"
+	"testing"
+)
+
+func TestRaw(t *testing.T) {
+	var out strings.Builder
+	var component = Raw(`text`)
+	var expect = `text`
+
+	if _, err := component.WriteNode(&out); err != nil {
+		t.Errorf(`failed to write component string: %+v`, err)
+		return
+	}
+
+	if out.String() != expect {
+		t.Errorf(`unexpected value: %+v => %+v != %+v`, `text`, out.String(), expect)
+	}
+}
 
 func TestText(t *testing.T) {
-	tests := [][2]string{
-		{string(Raw(`hello`)), `hello`},
-		{string(Text(`hello&workd`)), `hello&amp;workd`},
+	var out strings.Builder
+	var tests = [][2]string{
+		{`&amp;`, `&`},
+		{`&gt;`, `>`},
+		{`&lt;`, `<`},
+		{`&#34;`, `"`},
+		{`&#39;`, `'`},
+		{`&#96;`, "`"},
+		{`&#123;`, `{`},
+		{`&#125;`, `}`},
 	}
 
 	for _, test := range tests {
-		if test[0] != test[1] {
-			t.Errorf(`compare values mismatch: %v != %v`, test[0], test[1])
+		out.Reset()
+
+		raw := test[1]
+		escaped := test[0]
+
+		component := Text(raw)
+
+		if _, err := component.WriteNode(&out); err != nil {
+			t.Errorf(`failed to write component string: %+v`, err)
+			continue
 		}
+
+		if out.String() != escaped {
+			t.Errorf(`unexpected value: %+v => %+v != %+v`, raw, out.String(), escaped)
+			continue
+		}
+
+		value := html.UnescapeString(escaped)
+		if value != raw {
+			t.Errorf(`escape string mismatch: %+v => %+v !=  %+v`, escaped, raw, value)
+			continue
+		}
+
 	}
 }
 
 func TestAttr(t *testing.T) {
-	tests := [][2]string{
-		{string(Flag(`crossorigin`)), `crossorigin`},
-		{string(Flag(`cross&origin`)), `cross&amp;origin`},
+	var out strings.Builder
 
-		{string(Pair(`id`, `foo`)), `id="foo"`},
-		{string(Pair(`i&d`, `f&o`)), `i&amp;d="f&amp;o"`},
+	var single = [][2]string{
+		{`crossorigin`, `crossorigin`},
+		{`cross&origin`, `cross&amp;origin`},
 	}
 
-	for _, test := range tests {
-		if test[0] != test[1] {
-			t.Errorf(`compare values mismatch: %v != %v`, test[0], test[1])
+	for _, test := range single {
+		out.Reset()
+
+		from := test[0]
+		to := test[1]
+
+		component := Attr(from)
+
+		_, err := component.WriteNode(&out)
+		if err != nil {
+			t.Errorf(`failed to write component string: %+v`, err)
+			continue
+		}
+
+		if out.String() != to {
+			t.Errorf(`unexpected value: %+v => %+v != %+v`, from, out.String(), to)
+			continue
+		}
+	}
+
+	var pairs = [][3]string{
+		{`id`, `msg`, `id="msg"`},
+		{`i&d`, `msg`, `i&amp;d="msg"`},
+		{`id`, `m&g`, `id="m&amp;g"`},
+		{`i&d`, `m&g`, `i&amp;d="m&amp;g"`},
+	}
+
+	for _, test := range pairs {
+		out.Reset()
+
+		k := test[0]
+		v := test[1]
+		expect := test[2]
+
+		component := Attr(k, v)
+		_, err := component.WriteNode(&out)
+		if err != nil {
+			t.Errorf(`failed to write component string: %+v`, err)
+			continue
+		}
+
+		if out.String() != expect {
+			t.Errorf(`unexpected value: (%+v, %+v) => %+v != %+v`, k, v, out.String(), expect)
+			continue
 		}
 	}
 }
 
-func TestNode(t *testing.T) {
-	tests := [][2]string{
-		{string(Node(`hr`)), `<hr />`},
-		{string(Node(`hr`, Pair(`id`, `foo`))), `<hr id="foo" />`},
-		{string(Node(`p`, Text(`hello`))), `<p >hello</p>`},
-		{string(Node(`p`, Pair(`class`, `msg`), Text(`hi,`))), `<p class="msg">hi,</p>`},
+func TestElement(t *testing.T) {
+	var out strings.Builder
+	var tests = []struct {
+		el     NodeWriter
+		expect string
+	}{
+		{Element(`hr`), `<hr />`},
+		{Element(`h&r`), `<h&amp;r />`},
+		{Element(`hr`, Attr(`id`, `msg`)), `<hr id="msg" />`},
+		{Element(`hr`, Attr(`id`, `msg`), Attr(`class`, `hr`)), `<hr class="hr" id="msg" />`},
+		{Element(`p`, Text(`hello`), Text(`, `), Text(`world!`)), `<p>hello, world!</p>`},
+		{Element(`p&p`, Text(`hello`), Text(`, `), Text(`world!`)), `<p&amp;p>hello, world!</p&amp;p>`},
+		{Element(`p`, Attr(`id`, `msg`), Attr(`class`, `highlight`), Text(`hello`), Text(`, `), Text(`world!`)), `<p class="highlight" id="msg">hello, world!</p>`},
+		{Element(`p`, Element(`strong`, Text(`hi,`))), `<p><strong>hi,</strong></p>`},
 	}
 
 	for _, test := range tests {
-		if test[0] != test[1] {
-			t.Errorf(`compare values mismatch: %v != %v`, test[0], test[1])
+		out.Reset()
+		el := test.el
+		expect := test.expect
+
+		if _, err := el.WriteNode(&out); err != nil {
+			t.Errorf(`failed to write component string: %+v`, err)
+			continue
 		}
+
+		if out.String() != expect {
+			t.Errorf(`unexpected value: %+v != %+v`, out.String(), expect)
+		}
+	}
+}
+
+func BenchmarkRaw(b *testing.B) {
+	var out strings.Builder
+	var component = Raw(`test`)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for idx := 0; idx < b.N; idx++ {
+		component.WriteNode(&out)
+	}
+}
+
+func BenchmarkText(b *testing.B) {
+	var out strings.Builder
+	var component = Text(`test`)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for idx := 0; idx < b.N; idx++ {
+		component.WriteNode(&out)
+	}
+}
+
+func BenchmarkAttr(b *testing.B) {
+	var out strings.Builder
+	var single = Attr(`crossorigin`)
+	var pair = Attr(`id`, `msg`)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for idx := 0; idx < b.N; idx++ {
+		single.WriteNode(&out)
+		pair.WriteNode(&out)
+	}
+}
+
+func BenchmarkElement(b *testing.B) {
+	var out strings.Builder
+	var p = Element(`p`, Element(`strong`, Text(`hi, `), Text(`this is test message.`)))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for idx := 0; idx < b.N; idx++ {
+		p.WriteNode(&out)
+	}
+}
+
+func BenchmarkRealCase(b *testing.B) {
+	var out strings.Builder
+	var t = Element(`html`,
+		Element(`body`,
+			Element(`h1`, Text(`title`)),
+			Element(`p`, Text(`This is an example page!`)),
+			Element(`ul`,
+				Element(`li`, Text(`foo`),
+					Element(`li`, Text(`bar`),
+						Element(`li`, Text(`baz`)))))))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for idx := 0; idx < b.N; idx++ {
+		t.WriteNode(&out)
 	}
 }
